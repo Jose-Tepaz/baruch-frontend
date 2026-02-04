fee67e5b92e4bccbbedb6d98c0390111c9a6a6aa
Jose Tepaz <josetepaz28@gmail.com>
2026-02-04 10:03:20 -0600

    agregando redirección 301 para url services

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,15 @@
 const nextConfig = {
     // Configuración para App Router
     // No necesitamos la configuración i18n aquí ya que usamos middleware
+    async redirects() {
+        return [
+            {
+                source: '/services/sell-your-house-in-costa-del-sol',
+                destination: '/services/sell-your-house',
+                permanent: true,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
